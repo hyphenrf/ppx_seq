8e8756f42dc6101484c8d999ea09859505d3de65
Haz <hyphens@pm.me>
2022-01-26 21:37:42 +0000

    List.of_seq is undefined for OCaml < 4.07

--- test/ppx_seq.ml
+++ test/ppx_seq.ml
@@ -1,7 +1,11 @@
+let rec tolist xs = match xs () with
+  | Seq.Nil -> []
+  | Seq.Cons(x, xs) -> x :: tolist xs
+
 let tests = ((* BEGIN TESTS *))
 
   (* A sequence *)
-  ; assert begin List.of_seq [%seq 1;2;3] = [1;2;3] end
+  ; assert begin tolist [%seq 1;2;3] = [1;2;3] end
 
   (* Just a Nil thunk, equivalent to Seq.empty *)
-  ; assert begin List.of_seq [%seq.empty] = [] end
+  ; assert begin tolist [%seq.empty] = [] end
