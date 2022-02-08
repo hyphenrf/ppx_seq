0d6e1243d9db6862149c52e7d20d2ed29f6ed2a3
Haz <hyphens@pm.me>
2022-02-08 20:55:02 +0200

    Add [%seq.inf a[, b]] for Infinite Ranges

--- lib/ppx_seq.ml
+++ lib/ppx_seq.ml
@@ -15,8 +15,25 @@ let extend_seq = Extension.V2.declare "seq"
   )
   (fun ~loc ~path:_ xs -> eseq ~loc @@ Option.value ~default:[] xs)
 
-let rules = [
-  Context_free.Rule.extension extend_seq;
+let einf ~loc a b =
+  let mkgen body =
+    [%expr let rec gen x s () = Seq.Cons(x, gen (x + s) s) in [%e body]]
+  in
+  match a with
+  | None -> mkgen [%expr gen [%e b] (succ [%e b] - [%e b]) ]
+  | Some a -> mkgen [%expr gen [%e a] ([%e b] - [%e a]) ]
+
+let extend_inf = Extension.V2.declare "seq.inf"
+  Extension.Context.expression
+  Ast_pattern.(pstr @@ alt_option
+    (pstr_eval (pexp_tuple (__ ^:: __ ^:: nil)) nil ^:: nil)
+    (pstr_eval __ nil ^:: nil)
+  )
+  (fun ~loc ~path:_ -> einf ~loc)
+
+let rules = List.map Context_free.Rule.extension [
+  extend_seq;
+  extend_inf;
 ]
 
 let _ = Driver.register_transformation
--- test/ppx_seq.ml
+++ test/ppx_seq.ml
@@ -3,7 +3,13 @@ let rec tolist xs = match xs () with
   | Seq.Nil -> []
   | Cons(x,xs) -> x :: tolist xs
 
+let rec take n s () = match s() with
+  | Seq.Cons(x, xs) when n > 0 -> Seq.Cons(x, take (n-1) xs)
+  | _ -> Seq.Nil
+
 let tests = ((* BEGIN TESTS *))
+
+  (* Basics *)
 
   (* A sequence *)
   ; assert begin tolist [%seq 1;2;3] = [1;2;3] end
@@ -17,3 +23,14 @@ let tests = ((* BEGIN TESTS *))
       let _ = [%seq r := 0] in
          !r = 42
     end
+
+  (* Infinite Ranges *)
+
+  (* [a, succ a..] *)
+  ; assert begin tolist @@ take 3 [%seq.inf 1] = [1;2;3] end
+
+  (* [a, a+n, a+2n..] *)
+  ; assert begin tolist @@ take 3 [%seq.inf 1, 3] = [1;3;5] end (* n > 0 *)
+  ; assert begin tolist @@ take 3 [%seq.inf 1, 1] = [1;1;1] end (* n = 0 *)
+  ; assert begin tolist @@ take 3 [%seq.inf 5, 3] = [5;3;1] end (* n < 0 *)
+
